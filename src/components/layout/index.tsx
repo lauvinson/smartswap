3e91c364e1f81b449245f22c79bb2220515f905c
vinson <liuws@zhiyun-tech.com>
2023-09-01 11:14:37 +0800

    optimi

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -4,7 +4,6 @@ import { Header } from './Header'
 import { Footer } from './Footer'
 import { NetworkStatus } from './NetworkStatus'
 import { THEME_COLOR_SCHEME } from '../../utils/config'
-import { BrowserRouter } from 'react-router-dom'
 
 interface Props {
   children: ReactNode
@@ -12,18 +11,16 @@ interface Props {
 
 export function Layout(props: Props) {
   return (
-    <BrowserRouter>
-      <Box margin="0 auto" minH="100vh" bg={useColorModeValue(`${THEME_COLOR_SCHEME}.100`, `${THEME_COLOR_SCHEME}.900`)}>
-        <Header />
+    <Box margin="0 auto" minH="100vh" bg={useColorModeValue(`${THEME_COLOR_SCHEME}.100`, `${THEME_COLOR_SCHEME}.900`)}>
+      <Header />
 
-        <Container maxW="container.lg">{props.children}</Container>
+      <Container maxW="container.lg">{props.children}</Container>
 
-        <Box position="fixed" bottom={2} right={2}>
-          <NetworkStatus />
-        </Box>
-
-        <Footer />
+      <Box position="fixed" bottom={2} right={2}>
+        <NetworkStatus />
       </Box>
-    </BrowserRouter>
+
+      <Footer />
+    </Box>
   )
 }
