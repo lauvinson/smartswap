5715ac48d274d85da2e58740442addd32cd29b4e
vinson <wincentlauvinson@gmail.com>
2023-09-11 23:21:52 +0800

    Refactor Banner layout for better responsiveness
    Updated the classnames used in the Banner component of the Layout to improve responsiveness. The changes ensure proper positioning and alignment of elements on medium-sized screens and above, enhancing user's visual experience on different devices.

--- src/components/layout/Banner.tsx
+++ src/components/layout/Banner.tsx
@@ -15,13 +15,13 @@ export function HomeBanner(props: Props) {
   }
 
   return (
-    <div className={clsx('grid gap-4 grid-cols-1 grid-rows-2', className)}>
-      <div className="inline-grid">
+    <div className={clsx('grid gap-4 md:grid-cols-3 grid-cols-1 grid-rows-2', className)}>
+      <div className="inline-grid col-start-1 col-end-2 row-start-1 row-end-2 text-center md:text-left">
         <div className="text-3xl md:text-5xl font-bold">
           Put your funds <br /> to work by <br /> providing liquidity.
         </div>
       </div>
-      <div className="inline-grid">
+      <div className="inline-grid col-start-1 col-end-3 row-start-2 row-end-3 text-center md:text-left">
         <div className="text-large md:text-2xl font-light">
           Providing liquidity to a pool allows you to earn a percentage of the pools traded volume as well as any extra rewards if the pool is
           incentivized.
