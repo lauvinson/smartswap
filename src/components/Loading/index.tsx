540dd9e1a41caab319d1eedc3c6e5b55daeb7a5e
vinson <wincentlauvinson@gmail.com>
2023-09-13 00:08:06 +0800

    Optimize loading and responsiveness of application elements
    Refactored _app.tsx, using dynamic imports for certain components and providers. This change enhances performance by split these parts of the code into separate chunks that are loaded when needed, which results in faster initial page loading.
    
    Created a new 'LoadingPage' component, which is displayed while the dynamically imported components and providers are being loaded, delivering a better user experience during wait times.
    
    Commented out unused code in Header.tsx to improve readability, while the Navbar element was updated for better UI responsiveness.
    
    These changes are aimed at enhancing the user's visual experience on different devices and improving overall application performance.

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Logo from '@/assets/logo/color.svg'
+import { SITE_NAME } from '@/utils/config'
+import { motion } from 'framer-motion'
+
+export function LoadingPage() {
+  return (
+    <div className="flex justify-center items-center align-middle h-screen w-full">
+      <motion.div
+        initial={{ y: -10 }}
+        animate={{ y: 10 }}
+        transition={{
+          y: {
+            duration: 0.5,
+            repeat: Infinity,
+            repeatType: 'reverse',
+          },
+        }}>
+        <img draggable="false" width={50} height={50} src={Logo.src} alt={SITE_NAME} />
+      </motion.div>
+    </div>
+  )
+}
